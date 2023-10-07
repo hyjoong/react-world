c574b6dd52a4cb618461d0ac92d07a17d2aedb4c
henry <70426440+hyjoong@users.noreply.github.com>
2023-10-07 21:30:24 +0900

    feat: sign-in page layout 구현

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,43 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
+import { Button } from '@/components/ui/Button';
 
 const SignInPage = () => {
-  return <div>Sign In Page</div>;
+  const [formData, setFormData] = useState({ email: '', password: '' });
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="email"
+            name="email"
+            placeholder="email"
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            name="password"
+            placeholder="password"
+            onChange={handleInputChange}
+          />
+        </div>
+        <Button>login</Button>
+      </form>
+    </div>
+  );
 };
 
 export default SignInPage;
