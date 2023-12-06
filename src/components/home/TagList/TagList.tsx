22a77bd81f008caf42247bdd10a33e813f8f7873
hyjoong <maplesosser@naver.com>
2023-12-06 23:39:40 +0900

    feat: enable tag navigation

--- src/components/home/TagList/TagList.tsx
+++ src/components/home/TagList/TagList.tsx
@@ -1,4 +1,5 @@
 import { Badge } from '@/components/ui/Badge/Badge';
+import Link from 'next/link';
 
 export interface TagListProps {
   tags: string[];
@@ -6,12 +7,14 @@ export interface TagListProps {
 
 export const TagList = async ({ tags }: TagListProps) => {
   return (
-    <div className="w-[700px] py-2">
+    <div className="min-w-[250px] py-2">
       <p className="text-xl font-bold mb-3">Popular Tags</p>
       {tags?.map((tag, index) => (
-        <Badge key={index} variant="outline" className="mr-1">
-          {tag}
-        </Badge>
+        <Link href={`/tag/${tag}`}>
+          <Badge key={index} variant="outline" className="mr-1">
+            {tag}
+          </Badge>
+        </Link>
       ))}
     </div>
   );
