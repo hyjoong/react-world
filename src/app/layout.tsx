e03ccbbabaad7c3c05ff8b53d52ae8c65663ce3c
hyjoong <maplesosser@naver.com>
2023-12-09 21:56:20 +0900

    feat: reactQueryProvider setting 및 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { ReactNode } from 'react';
 import { Navbar } from '@/components/layout/Navbar';
 import { ThemeProvider } from '@/components/providers/ThemeProvider';
 import { MSWProvider } from '@/mocks/MSWProvider';
+import ReactQueryProviders from '@/components/providers/ReactQueryProvider';
 
 export const metadata: Metadata = {
   title: 'react world',
@@ -15,12 +16,14 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="ko" suppressHydrationWarning>
       <body>
         <MSWProvider>
+          <ReactQueryProviders>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             <div className="container">
               <Navbar />
               {children}
             </div>
           </ThemeProvider>
+          </ReactQueryProviders>
         </MSWProvider>
       </body>
     </html>
